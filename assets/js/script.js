2229e7807772a38f1723ee276b10daa290d34abb
Nurul Hidayatullah <nurulh298hd@gmail.com>
2022-08-20 19:30:58 +0700

    menambahkan content theory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
+// Javascript untuk togle menu
 const menu = document.getElementById('menu');
 menu.onclick = function () {
   menu.classList.toggle('openmenu');
 };
+
+// Javascript untuk slide isi togle menu
 const menuToggle = document.getElementById('menu');
 const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function () {
